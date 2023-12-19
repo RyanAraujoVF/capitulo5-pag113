303d45e19ad3c2de6c332cffdea686c00b9ec5ea
RyanAraujoVF <153670118+RyanAraujoVF@users.noreply.github.com>
2023-12-19 03:03:54 -0300

    SomaDeSerieQuestão9.java

--- SomaDeSerieQuestão9.java
+++ SomaDeSerieQuestão9.java
@@ -1,20 +1,23 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
 package somadeserie.questão9;
 
-/**
- *
- * @author Sandro3
- */
+import javax.swing.JOptionPane;
+
 public class SomaDeSerieQuestão9 {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
-        // TODO code application logic here
+    int n, i;
+    double soma, termo;
+    
+    String input = JOptionPane.showInputDialog("Digite a quantidade de termos da série:");
+    n = Integer.parseInt(input);
+    soma = 0;
+   
+    for (i = 1; i <= n; i++) 
+    {
+        termo = i / (11.0 * i + 1);
+        soma += termo;
     }
     
+    JOptionPane.showMessageDialog(null, "A soma da série é: " + soma);
+   }
 }
