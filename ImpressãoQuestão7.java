ab44c40cf4388bedf4d9a31e295b271d59f08cea
RyanAraujoVF <153670118+RyanAraujoVF@users.noreply.github.com>
2023-12-19 03:02:53 -0300

    ImpressãoQuestão7.java

--- ImpressãoQuestão7.java
+++ ImpressãoQuestão7.java
@@ -1,20 +1,30 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
 package impressão.questão7;
 
-/**
- *
- * @author Sandro3
- */
+import javax.swing.JOptionPane;
+
 public class ImpressãoQuestão7 {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
-        // TODO code application logic here
+     // Declarar as variáveis
+    int X, I;
+    // Inicializar a variável I com 1
+    I = 1;
+    // Criar uma variável para armazenar o texto a ser exibido
+    String texto = "";
+    // Enquanto I for menor que 20, repetir o bloco de instruções
+    while (I < 20) {
+        // Calcular o quadrado de I e atribuir à variável X
+        X = I * I;
+        // Adicionar o valor de I e X ao texto, seguido de uma quebra de linha
+        texto += I + " " + X + "\n";
+        // Incrementar I em 2
+        I = I + 2;
+        // Adicionar uma mensagem ao texto, seguida de uma quebra de linha
+        texto += "Passei por aqui!\n";
     }
-    
+    // Adicionar uma mensagem final ao texto
+    texto += "Ufa, cheguei aqui!";
+    // Mostrar o texto em uma caixa de diálogo
+    JOptionPane.showMessageDialog(null, texto);
+   }
 }
